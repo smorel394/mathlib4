25b12cf63ed14471c54290169b6e146389c29d8f
morel <sophie.morel@ens-lyon.fr>
2026-07-09 18:38:22 +0200

    start merge

--- Mathlib/CategoryTheory/Preadditive/FreydCategory/RightFreyd.lean
+++ Mathlib/CategoryTheory/Preadditive/FreydCategory/RightFreyd.lean
@@ -99,7 +99,7 @@ lemma quotient_map_eq_zero_iff : (quotient V).map f = 0 ↔ Nonempty (RightHomot
 
 /-- If `f` is a morphism of `Arrow V` such that `f.right` is an isomorphism, then the image of `f`
 in the right Freyd category is an epimorphism. -/
-lemma isEpi_of_right_iso [IsIso f.right] : Epi ((quotient V).map f) where
+lemma epi_of_isIso_right [IsIso f.right] : Epi ((quotient V).map f) where
   left_cancellation g₁ g₂ eq := by
     obtain ⟨g₁, rfl⟩ := (quotient V).map_surjective g₁
     obtain ⟨g₂, rfl⟩ := (quotient V).map_surjective g₂
@@ -240,9 +240,9 @@ def condition : RightHomotopy (f ≫ π f) 0 where
   comm := by simp [π]
 
 set_option backward.isDefEq.respectTransparency false in
-instance isEpi_π : Epi ((quotient V).map (π f)) :=
+instance : Epi ((quotient V).map (π f)) :=
   have : IsIso ((π f).right) := by simp only [π, homMk_right]; infer_instance
-  isEpi_of_right_iso _
+  epi_of_isIso_right _
 
 variable {w : Arrow V} (g : v ⟶ w) (h : RightHomotopy (f ≫ g) 0)
 
@@ -252,53 +252,35 @@ homotopic to `0`, this is the morphism from the "candidate cokernel" of `f` to `
 from the right homotopy. -/
 def desc : cokernel f ⟶ w :=
   Arrow.homMk (biprod.desc g.left h.hom) g.right (biprod.hom_ext' _ _ (by simp)
-  (by simp [h.comm.symm]))
+    (by simp [← h.comm]))
 
 set_option backward.isDefEq.respectTransparency false in
-@[reassoc]
+@[reassoc (attr := simp)]
 lemma π_desc : π f ≫ desc f g h = g := by ext <;> simp [π, desc]
 
-end Candidate
-
-variable {X : RightFreyd V} {a : (quotient V).obj v ⟶ X} (eq : (quotient V).map f ≫ a = 0)
-
-/-- Let `f : u ⟶ v` be a morphism in `Arrow V`, and let `a : (quotient V).obj v ⟶ X` be
-a morphism in `RightFreyd V` such that `(quotient V).map f ≫ a = 0`. This is the morphism
-`(quotient V).obj (Candidate.cokernel f) ⟶ X` that will serve as `cokernel.desc f`. -/
-def desc : (quotient V).obj (Candidate.cokernel f) ⟶ X := by
-  rw [← ((quotient V).map_surjective a).choose_spec] at eq
-  exact (quotient V).map (Candidate.desc _ _ (homotopyOfEq _ _ eq))
-
-lemma π_desc : (quotient V).map (Candidate.π f) ≫ desc f eq = a := by
-  rw [← ((quotient V).map_surjective a).choose_spec] at eq
-  conv_rhs => rw [← ((quotient V).map_surjective a).choose_spec,
-                      ← Candidate.π_desc _ _ (homotopyOfEq _ _ eq)]
-  rfl
-
 /-- For `f` a morphism in `Arrow V`, this is a cokernel cofork of `(quotient V).map f`. -/
-def CandidateCokernelCofork : CokernelCofork ((quotient V).map f) :=
+def cokernelCofork : CokernelCofork ((quotient V).map f) :=
   CokernelCofork.ofπ ((quotient V).map (Candidate.π f))
-  (eq_of_rightHomotopy _ _ (Candidate.condition f))
+    (eq_of_rightHomotopy _ _ (Candidate.condition f))
 
 set_option backward.isDefEq.respectTransparency false in
 /-- For `f` a morphism in `Arrow V`, the cokernel cofork of `(quotient V).map f` constructed
-in `CandidateCokernelCofork` is a colimit cofork. -/
-def candidateCokernelCoforkIsCokernel : IsColimit (CandidateCokernelCofork f) where
-  desc s := desc f (CokernelCofork.condition s)
-  fac s j :=
-    match j with
-    | WalkingParallelPair.zero => by simp
-    | WalkingParallelPair.one => π_desc f (CokernelCofork.condition s)
-  uniq s m eq :=
-    (cancel_epi ((quotient V).map (Candidate.π f))).mp ((eq WalkingParallelPair.one).trans
-    (π_desc f (CokernelCofork.condition s)).symm)
+in `cokernelCofork` is a colimit cofork. -/
+def isColimitCokernelCofork : IsColimit (cokernelCofork f) :=
+  CokernelCofork.IsColimit.ofπ' _
+    (eq_of_rightHomotopy _ _ (Candidate.condition f))
+    (fun g hg ↦ Nonempty.some (by
+      obtain ⟨g, rfl⟩ := (quotient V).map_surjective g
+      exact ⟨(quotient V).map (desc f g (homotopyOfEq _ _ hg)),
+        by simp [← Functor.map_comp]⟩))
 
-/-- The category `RightFreyd V` has all cokernels if `V` has finite products. -/
+end Candidate
+
+/-- The category `RightFreyd V` has all cokernels if `V` has binary biproducts. -/
 instance : HasCokernels (RightFreyd V) where
-  has_colimit {X Y} f := {
-    exists_colimit := by
-      obtain ⟨f, rfl⟩ := (quotient V).map_surjective f
-      exact Nonempty.intro ⟨CandidateCokernelCofork f, candidateCokernelCoforkIsCokernel f⟩ }
+  has_colimit f := ⟨by
+    obtain ⟨f, rfl⟩ := (quotient V).map_surjective f
+    exact ⟨_, Candidate.isColimitCokernelCofork f⟩⟩
 
 variable [HasZeroObject V]
 
@@ -324,7 +306,7 @@ set_option backward.isDefEq.respectTransparency false in
 def coforkRightFunctorIsColimit (u : Arrow V) : IsColimit (coforkRightFunctor u) := by
   have : Epi ((quotient V).map (Arrow.homMk (f := Arrow.mk 0) (g := Arrow.mk u.hom)
       (0 : 0 ⟶ u.left) (𝟙 u.right) (by simp))) := by
-    apply +allowSynthFailures isEpi_of_right_iso
+    apply +allowSynthFailures epi_of_isIso_right
     exact inferInstanceAs (IsIso (𝟙 _))
   refine CokernelCofork.IsColimit.ofπ' _ zero (fun f eq ↦ ?_)
   set t := (quotient V).map_surjective f
